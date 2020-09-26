7beb40dcd2500d302de163be86b901045e6d3179
Michael Richters <gedankenexperimenter@gmail.com>
2020-09-26 11:24:44 -0500

    Add new EAF summarization script `summarize-eaf.py`
    This new version of the counter script does a better job of detecting
    overlaps, particularly when more than two tiers are active
    simultaneously. `summarize-eaf.py` collects all of the annotated
    segments of interest, and generates a list of "events" (times when the
    segments start or end) from them, then puts all these events in
    chronological order. This technique produces a more accurate union sum
    of the amount of time the collective set of tiers is active.
    
    In addition, the output table has been changed to make it more
    machine-readable. The columns are now consistent across an entire
    corpus, and each row belongs to a unique set of simultaneously active
    tiers. It should be possible to use spreadsheet formulas or other
    programs on these output tables more easily to do further analyses.
    
    The command-line argument parser has many more options, making it easy
    to exclude any tiers from the analysis, change the output field
    delimiter, and make the output briefer if overlap details or ADS/CDS
    information are not needed.
    
    Last, the script runs ~20 times faster than the old one.

---
+++ summarize-eaf.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import argparse
+import csv
+import os
+import sys
+import warnings
+
+from collections import defaultdict
+
+import pympi  # Import for EAF file parsing
+
+# ==============================================================================
+# Class definitions
+# ------------------------------------------------------------------------------
+class Event:
+    """Represents either the beginning or the end of an annotated segment"""
+    def __init__(self, timestamp, label, start=True, annotation=''):
+        self.label      = label
+        self.annotation = annotation
+        self.timestamp  = timestamp
+        if start:
+            self.change =  1
+        else:
+            self.change = -1
+
+# ------------------------------------------------------------------------------
+class Segment:
+    """Represents an annotated segment from an EAF file"""
+    def __init__(self, tier, start_time, end_time, value):
+        self.tier       = tier
+        self.start_time = int(start_time)
+        self.end_time   = int(end_time)
+        self.value      = value
+
+# ------------------------------------------------------------------------------
+class OutputRecord:
+    """Represents a row of the data table to be written to the output file"""
+    data_labels = ['exclusive', 'total', 'cds', 'ads', 'both']
+    header = ['File', 'Tier(s)', 'Exclusive', 'Total', 'CDS', 'ADS', 'BOTH']
+
+    def __init__(self, file_id, label):
+        self.file_id = file_id
+        self.label = label
+        self.data = defaultdict(int)
+        return
+
+    def fmt(self):
+        values = [self.file_id, self.label]
+        def _blank_zero(entry):
+            value = self.data[entry]
+            return '' if value == 0 else value
+        data_values = map(_blank_zero, self.data_labels)
+        values.extend(data_values)
+        return values
+
+# ==============================================================================
+# Helper functions
+# ------------------------------------------------------------------------------
+def get_segments(eaf, tiers):
+    """
+    Extract a list of annotated segments for a set of tiers from an
+    EAF file object.
+    """
+    segments = []
+    for tier in tiers:
+        for record in eaf.get_annotation_data_for_tier(tier):
+            (start_time, end_time, value) = record[:3]
+            segments.append(Segment(tier, start_time, end_time, value))
+    return segments
+
+# ------------------------------------------------------------------------------
+def get_events(segments, label_func=lambda x: x.tier):
+    """
+    Given a list of `AnnotationSegment`s, return a sorted list of
+    `Event` objects.
+    """
+    events = []
+    for segment in segments:
+        # Start of segment
+        events.append(Event(timestamp  = segment.start_time,
+                            label      = label_func(segment),
+                            annotation = segment.value,
+                            start      = True))
+        # End of segment
+        events.append(Event(timestamp  = segment.end_time,
+                            label      = label_func(segment),
+                            annotation = segment.value,
+                            start      = False))
+    # Sort events chronologically
+    events.sort(key=lambda event: event.timestamp)
+    return events
+
+# ------------------------------------------------------------------------------
+def process_events(events, labels = []):
+    """Process a sorted list of `Event` objects."""
+
+    # Initialize return values
+    union_sum      = 0
+    sections       = []
+    section_sums   = defaultdict(int)
+    section_counts = defaultdict(int)
+
+    # Temporary loop variables
+    section_labels = []
+    # Ignore any uncategorized space before the first event
+    section_start = events[0].timestamp
+
+    for event in events:
+        # Any labels not included are ignored. Default is to consider
+        # all labels.
+        if labels and event.label not in labels:
+            continue
+
+        # We have reached the end of a section where a given set of
+        # labels was active (either a new one started, or an active one
+        # ended. We add the duration of the section to the appropriate
+        # combination of labels' total.
+        section_label = '+'.join(sorted(section_labels))
+        section_duration = event.timestamp - section_start
+        section_sums[section_label] += section_duration
+        section_counts[section_label] += 1
+        if section_duration > 0:
+            sections.append((section_label, section_duration))
+        if section_labels:
+            union_sum += section_duration
+
+        # Either a new label started, or an existing one ended. Either
+        # way, we need to update the list of current labels.
+        if event.change > 0:
+            section_labels.append(event.label)
+        else:
+            section_labels.remove(event.label)
+
+        # Now, if there are any active labels, set the timestamp to
+        # record the next section.
+        if section_labels:
+            section_start = event.timestamp
+
+    return union_sum, section_sums
+
+# ------------------------------------------------------------------------------
+def process_category(category, events, labels, output_records):
+    if len(events) == 0: return
+    for event in events:
+        event.label = event.label.split(':')[0]
+    (_, section_sums) = process_events(events)
+    for label in labels:
+        output_records[label].data[category] += section_sums[label]
+        output_records['totals'].data[category] += section_sums[label]
+    return
+
+# ==============================================================================
+# Command-line parser
+# ------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(
+    formatter_class = argparse.RawDescriptionHelpFormatter,
+    description = "Analyze and report the annotated time segments for tiers in EAF files.",
+    epilog =
+    "Examples:\n" +
+    "    {} -o foo.csv raw_FOO/*.eaf\n".format(__file__) +
+    "    {} --ignore-tiers EE1 UC1 -- raw_FOO/*.eaf\n\n".format(__file__) +
+    "[When using --ignore-tiers, separate tier names from EAF file names with '--'.]"
+)
+
+parser.add_argument('-o', '--output',
+                    metavar = '<csv_file>',
+                    type    = argparse.FileType('w'),
+                    default = 'eaf-counts.csv',
+                    help    = "Write output to <csv_file> (default: '%(default)s')")
+
+parser.add_argument('-d', '--delimiter',
+                    choices = ['tab', 'comma', 'ascii'],
+                    default = 'tab',
+                    help    = "Use <delimiter> as CSV output field separator (default: '%(default)s')")
+
+parser.add_argument('--ignore-tiers',
+                    dest    = 'ignore',
+                    metavar = '<tier>',
+                    nargs   = '+',
+                    default = [],
+                    help    = "List of one or more additional EAF tiers to ignore (space separated list)")
+
+parser.add_argument('--no-xds',
+                    dest    = 'xds',
+                    action  = 'store_false',
+                    help    = "Don't summarize ADS & CDS amounts")
+
+parser.add_argument('--no-overlap',
+                    dest    = 'overlap',
+                    action  = 'store_false',
+                    help    = "Don't include tier overlap details in output")
+
+parser.add_argument('eaf_files',
+                    metavar = '<eaf_file>',
+                    nargs   = '+',
+                    help    = "The name(s) of the EAF file(s) to process")
+
+args = parser.parse_args()
+
+# ==============================================================================
+# Finalize options, initialize output
+# ------------------------------------------------------------------------------
+ignored_tiers = ['code_num', 'on_off', 'context', 'code']
+ignored_tiers.extend(args.ignore)
+
+output_delimiter = '\t'
+if args.delimiter == 'comma':
+    output_delimiter = ','
+elif args.delimiter == 'ascii':
+    output_delimiter = '\x1f'
+
+# Set up output csv writer
+output = csv.writer(args.output,
+                    delimiter      = output_delimiter,
+                    quoting        = csv.QUOTE_MINIMAL,
+                    lineterminator = '\n')
+# Write headers
+output.writerow(OutputRecord.header)
+
+grand_totals = OutputRecord('Grand Totals', '')
+
+# ==============================================================================
+# Start processing EAF files
+# ------------------------------------------------------------------------------
+for eaf_file in args.eaf_files:
+    print('Processing {}'.format(eaf_file))
+    file_id = os.path.basename(eaf_file).replace('.eaf', '')
+
+    # Initialize the EAF file parser
+    warnings.filterwarnings('ignore')
+    # pympi issues a warning "Parsing unknown version of ELAN spec..."
+    eaf = pympi.Elan.Eaf(eaf_file)
+    warnings.filterwarnings('default')
+
+    # Get tier names from EAF file
+    tiers = eaf.get_tier_names()
+    # Filter out tiers with no sub-tiers
+    tiers = filter(lambda t: '@' in t, tiers)
+    # From those, extract the set of unique base tiers
+    tiers = sorted(set(map(lambda t: t.split('@')[-1], tiers)))
+    # Filter out ignored tiers
+    tiers = filter(lambda t: t not in ignored_tiers, tiers)
+
+    # Extract annotated segments from EAF
+    segments = get_segments(eaf, tiers)
+
+    # Convert segments (with start & end times) to events (with either
+    # a start or end timestamp, but not both)
+    events = get_events(segments)
+
+    # Calculate sums and overlap for each combination of tiers
+    (union_sum, section_sums) = process_events(events)
+
+    # Get list of tier combinations (e.g. `CHI+FA2`)
+    labels = sorted(section_sums.keys())
+    # Ignore gaps between annotated sections
+    labels.remove('')
+
+    # Create dictionary for storing output records, and add the record
+    # for storing the totals for the whole EAF
+    output_records = dict()
+    output_records['totals'] = OutputRecord(file_id, 'Totals')
+
+    # Iterate through the tier combinations found above, and add the
+    # total time that combination was the only one active
+    for label in labels:
+        output_records[label] = OutputRecord(file_id, label)
+        output_records[label].data['exclusive'] += section_sums[label]
+        output_records['totals'].data['exclusive'] += section_sums[label]
+
+    # For top-level tiers only, report a value in the `total` field
+    for tier in tiers:
+        for label in labels:
+            if tier in label:
+                output_records[tier].data['total'] += section_sums[label]
+                output_records['totals'].data['total'] += section_sums[label]
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # If we're reporting ADS & CDS data:
+    if args.xds:
+        # Get the list of tiers, including sub-tiers, but excluding
+        # the ignored ones
+        tiers = filter(lambda t: t not in ignored_tiers, eaf.get_tier_names())
+        # Narrow that list to only the tiers with ADS & CDS annotations
+        xds_tiers = filter(lambda t: 'xds@' in t, tiers)
+
+        # Extract annotated segment data for the XDS tiers
+        segments = get_segments(eaf, xds_tiers)
+
+        # Convert segments to events list, setting the event labels to
+        # the base tier name with the annotation code appended (for
+        # example: `xds@FA1` with a `C` code becomes `FA1:C`)
+        events = get_events(
+            segments, lambda x: x.tier.split('@')[-1] + ':' + x.value)
+
+        # Create filtered lists corresponding to ADS, CDS, and BOTH annotations
+        cds_events = filter(lambda x: ':C' in x.label or ':T' in x.label, events)
+        ads_events = filter(lambda x: ':A' in x.label, events)
+        both_events = filter(lambda x: ':B' in x.label, events)
+
+        # Process the categories, adding the extracted data to the
+        # corresponding output records
+        process_category('cds', cds_events, labels, output_records)
+        process_category('ads', ads_events, labels, output_records)
+        process_category('both', both_events, labels, output_records)
+
+    # Get the list of labels for all output records
+    labels = sorted(output_records.keys())
+    labels.remove('totals')
+
+    # Report on top-level tiers on their own first
+    for label in filter(lambda x: x in tiers, labels):
+        output.writerow(output_records[label].fmt())
+
+    # If it has been requested, report overlap details for each
+    # combination of tiers in the EAF file
+    if args.overlap:
+        for label in filter(lambda x: x not in tiers, labels):
+            output.writerow(output_records[label].fmt())
+
+    # Write the totals for the current EAF file
+    output.writerow(output_records['totals'].fmt())
+
+    # Update the Grand Totals data for the set of EAF files being processed
+    for category in output_records['totals'].data.keys():
+        grand_totals.data[category] += output_records['totals'].data[category]
+
+# ------------------------------------------------------------------------------
+# Finally, write the Grand Totals row
+output.writerow(grand_totals.fmt())
+args.output.close()
+
+exit
