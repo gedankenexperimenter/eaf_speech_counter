fa54c778439958fbc3b2904f26f43ab71ea6936b
Michael Richters <gedankenexperimenter@gmail.com>
2020-09-28 13:54:31 -0500

    WIP cleanup

--- count-eaf-times.py
+++ count-eaf-times.py
@@ -51,9 +51,6 @@ class OutputRecord:
     """Represents a row of the data table to be written to the output file"""
     data_labels = ['exclusive', 'total', 'cds', 'ads', 'both']
     header = ['File', 'Tier(s)', 'Exclusive', 'Total', 'CDS', 'ADS', 'BOTH']
-    # @staticmethod
-    # def header(sep):
-    #     return sep.join(['File', 'Tier(s)', 'Exclusive', 'Total', 'CDS', 'ADS', 'BOTH'])
 
     def __init__(self, file_id, label):
         self.file_id = file_id
@@ -65,19 +62,10 @@ class OutputRecord:
         values = [self.file_id, self.label]
         def str_blank_zero(entry):
             value = self.data[entry]
-            return '' if value == 0 else str(value)
+            return '' if value == 0 else value
         data_values = map(str_blank_zero, self.data_labels)
         values.extend(data_values)
         return values
-
-    # def format(self, sep):
-    #     def str_blank_zero(entry):
-    #         value = self.data[entry]
-    #         return '' if value == 0 else str(value)
-    #     data_values = map(str_blank_zero, self.data_labels)
-    #     values = [self.file_id, self.label]
-    #     values.extend(data_values)
-    #     return sep.join(values)
 
 
 # ==============================================================================
@@ -122,7 +110,7 @@ def process_events(events, labels = []):
     # Temporary loop variables
     section_labels = []
     # Ignore any uncategorized space before the first event
-    section_start = events[1].timestamp
+    section_start = events[0].timestamp
 
     for event in events:
         # Any labels not included are ignored. Default is to consider
@@ -157,7 +145,8 @@ def process_events(events, labels = []):
     return union_sum, section_sums, sections
 
 # ------------------------------------------------------------------------------
-def process_category(category, events, output_records):
+def process_category(category, events, labels, output_records):
+    if len(events) == 0: return
     for event in events:
         event.label = event.label.split(':')[0]
     (union_sum, section_sums, sections) = process_events(events)
@@ -192,7 +181,7 @@ ignored_tiers = ['code_num', 'on_off', 'context', 'code']
 ignored_tiers.extend(args.ignore)
 
 output = csv.writer(args.totals, delimiter=args.sep,
-                    quotechar='"', quoting=csv.QUOTE_MINIMAL) 
+                    escapechar='\\', quoting=csv.QUOTE_MINIMAL) 
 output.writerow(OutputRecord.header)
 #args.totals.write(OutputRecord.header(args.sep) + '\n')
 
@@ -200,6 +189,7 @@ grand_totals = OutputRecord('Grand Totals', '')
 
 # ------------------------------------------------------------------------------
 for eaf_file in args.eaf_files:
+    print(eaf_file)
     file_id = os.path.basename(eaf_file).replace('.eaf', '')
     warnings.filterwarnings('ignore')
     # pympi issues a warning "Parsing unknown version of ELAN spec..."
@@ -227,12 +217,8 @@ for eaf_file in args.eaf_files:
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     if args.xds:
-        ads_total = 0
-        cds_total = 0
-        both_total = 0
-        tiers = filter(lambda tier: tier not in ignored_tiers,
-                       eaf.get_tier_names())
-        xds_tiers = filter(lambda tier: 'xds@' in tier, tiers)
+        tiers = filter(lambda t: t not in ignored_tiers, eaf.get_tier_names())
+        xds_tiers = filter(lambda t: 'xds@' in t, tiers)
         segments = get_records(eaf, xds_tiers)
         events = segments_to_events(
             segments, lambda x: x.tier.split('@')[-1] + ':' + x.value)
@@ -241,22 +227,23 @@ for eaf_file in args.eaf_files:
         ads_events = filter(lambda x: ':A' in x.label, events)
         both_events = filter(lambda x: ':B' in x.label, events)
 
-        process_category('cds', cds_events, output_records)
-        process_category('ads', ads_events, output_records)
-        process_category('both', both_events, output_records)
+        process_category('cds', cds_events, labels, output_records)
+        process_category('ads', ads_events, labels, output_records)
+        process_category('both', both_events, labels, output_records)
 
     labels = output_records.keys()
     labels.sort()
-    for label in labels:
+    for label in filter(lambda x: x in tiers, labels):
         output.writerow(output_records[label].fmt())
-        #args.totals.write(output_records[label].format(args.sep) + '\n')
+
+    for label in filter(lambda x: x not in tiers, labels):
+        output.writerow(output_records[label].fmt())
 
     for category in output_records['totals'].data.keys():
         grand_totals.data[category] += output_records['totals'].data[category]
 
 # ------------------------------------------------------------------------------
 output.writerow(grand_totals.fmt())
-#args.totals.write(grand_totals.format(args.sep) + '\n')
 args.totals.close()
 
 exit
