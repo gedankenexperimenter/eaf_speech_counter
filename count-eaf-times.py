d0d813810adf42c1e1622c1bdbed5554a581aaf0
Michael Richters <gedankenexperimenter@gmail.com>
2020-09-27 19:24:15 -0500

    WIP: mostly functional
    This does a much better job of overlap detection, reporting on all
    different sets of tier overlap. It doesn't do EE1 segregation,
    however.

--- count-eaf-times.py
+++ count-eaf-times.py
@@ -1,17 +1,29 @@
-
+#!/usr/bin/env python2
 from __future__ import print_function
-import sys
-import glob  # Import glob to easily loop over files
-import pympi  # Import for EAF file read
-import warnings
-from collections import defaultdict
-#from pathlib import Path
+
+import argparse
+import glob
 import os
+import sys
+import warnings
 
-# removing warnings
-warnings.filterwarnings("ignore")
+from collections import defaultdict
 
-class AnnotationRecord:
+import pympi  # Import for EAF file read
+
+
+class Event:
+    def __init__(self, timestamp, label, start=True, annotation=''):
+        self.label      = label
+        self.annotation = annotation
+        self.timestamp  = timestamp
+        if start:
+            self.change =  1
+        else:
+            self.change = -1
+
+
+class AnnotatedSegment:
     def __init__(self, tier, start_time, end_time, value):
         self.tier       = tier
         self.start_time = int(start_time)
@@ -22,7 +34,7 @@ class AnnotationRecord:
         return self.end_time - self.start_time
 
     def overlap(self, other):
-        first_end = min(self.end_time, other.end_time)
+        first_end  = min(self.end_time, other.end_time)
         last_start = max(self.start_time, other.start_time)
         if first_end > last_start:
             return first_end - last_start
@@ -30,227 +42,159 @@ class AnnotationRecord:
             return 0
 
 
-# if we fix tier names
-# tier_names = ['FA1', 'FA2','CHI']#code_num,on_off, context, code
-ignore_tier_names=['code_num', 'on_off', 'context', 'code']
+def get_records(eaf, tiers):
+    segments = []
+    for tier in tiers:
+        for record in eaf.get_annotation_data_for_tier(tier):
+            (start_time, end_time, value) = record[:3]
+            segments.append(AnnotatedSegment(tier, start_time,
+                                             end_time, value))
+    return segments
 
-#if the output file exists, delete it!
-if os.path.isfile(sys.argv[2]):
-    os.remove(sys.argv[2])
+def segments_to_events(segments, label_func=lambda x: x.tier):
+    """
+    Given a list of `AnnotationSegment`s, return a sorted list of
+    `Event` objects.
+    """
+    events = []
+    for segment in segments:
+        events.append(Event(timestamp = segment.start_time,
+                            label = label_func(segment),
+                            annotation = segment.value,
+                            start = True))
+        events.append(Event(timestamp = segment.end_time,
+                            label = label_func(segment),
+                            annotation = segment.value,
+                            start = False))
+    events.sort(key=lambda event: event.timestamp)
+    return events
 
+def process_events(events, labels = []):
+    """Process a sorted list of `Event` objects."""
+    # Initialize return values
+    union_sum     = 0
+    sections      = []
+    section_sums  = defaultdict(int)
 
-ads_total=0
-cds_total=0
-both_total=0
-grand_total_accross=0
-total_intersection_accross=0
-adjusted_total_accross=0
-# loop over all the EAF files in the output folder
-for file_path in glob.glob('{}/*.eaf'.format(sys.argv[1])):
-    tier_names = []
-    tier_names_targets = []
-    tier_dictionary = defaultdict(list)
-    annotation_records = []
-    grand_total = 0
-    print('File Name: {}'.format(os.path.basename(file_path)))
-    # Initialize the elan file
-    eafob = pympi.Elan.Eaf(file_path)
+    # Temporary loop variables
+    section_labels = []
+    # Ignore any uncategorized space before the first event
+    section_start = events[1].timestamp
 
-    # get the name of all the tiers available in the EAF file
-    tier_names = eafob.get_tier_names()
-    print(tier_names)
-
-    #loop over all the Tiers
-    for ort_tier in tier_names:
-        if ort_tier in ignore_tier_names:
+    for event in events:
+        # Any labels not included are ignored. Default is to consider
+        # all labels.
+        if labels and event.label not in labels:
             continue
-        # if "@" not in ort_tier:
-        #     tier_names_targets.append(ort_tier)
-        #     continue
-        tier_name = ort_tier.split('@')[-1]
-        if tier_name and tier_name not in tier_names_targets:
-            tier_names_targets.append(tier_name)
 
-    #sort the tier names 
-    tier_names_targets.sort()
+        # We have reached the end of a section where a given set of
+        # labels was active (either a new one started, or an active one
+        # ended. We add the duration of the section to the appropriate
+        # combination of labels' total.
+        section_label = '+'.join(sorted(section_labels))
+        section_duration = event.timestamp - section_start
+        section_sums[section_label] += section_duration
+        if section_duration > 0:
+            sections.append((section_label, section_duration))
+        if section_labels:
+            union_sum += section_duration
 
-    cds_totals = defaultdict(int)
-    ads_totals = defaultdict(int)
-    bds_totals = defaultdict(int)
-    for tier_name in tier_names_targets:
-        if tier_name in ignore_tier_names:
-            continue
-        for annotation in eafob.get_annotation_data_for_tier(tier_name):
-            (start_time, end_time, value) = annotation[:3]
-            annotation_records.append(
-                AnnotationRecord(tier_name, start_time, end_time, value))
+        # Either a new label started, or an existing one ended. Either
+        # way, we need to update the list of current labels.
+        if event.change > 0:
+            section_labels.append(event.label)
+        else:
+            section_labels.remove(event.label)
 
-    for full_tier_name in tier_names:
-        if "xds@" not in full_tier_name:
-            continue
-        tier_name = full_tier_name.split('@')[-1]
-        for annotation in eafob.get_annotation_data_for_tier(full_tier_name):
-            (start_time, end_time, value) = annotation[:3]
-            if value == "C" or value == "T":
-                cds_totals[tier_name] += end_time - start_time
-            elif value == "A":
-                ads_totals[tier_name] += end_time - start_time
-            elif value == "B":
-                bds_totals[tier_name] += end_time - start_time
+        # Now, if there are any active labels, set the timestamp to
+        # record the next section.
+        if section_labels:
+            section_start = event.timestamp
 
-    for tier_name in tier_names_targets:
-        print(tier_name + ".CDS: " + str(cds_totals[tier_name]))
-        print(tier_name + ".ADS: " + str(ads_totals[tier_name]))
-        print(tier_name + ".Both: " + str(bds_totals[tier_name]))
-
-    duration_total = 0
-    overlap_total = 0
-
-    overlap_matrix = dict()
-    for tier_name in tier_names_targets:
-        overlap_matrix[tier_name] = defaultdict(int)
-        # for tn2 in tier_names_targets:
-        #     overlap_matrix[tn1][tn2] = 0
-
-    duration_totals = defaultdict(int)
-    for i, ar1 in enumerate(annotation_records, start = 1):
-        duration_totals[ar1.tier] += ar1.duration()
-        duration_total += ar1.duration()
-        for ar2 in annotation_records[i:]:
-            overlap_duration = ar1.overlap(ar2)
-            overlap_matrix[ar1.tier][ar2.tier] += overlap_duration
-            overlap_total += overlap_duration
-            # overlap_matrix[ar2.tier][ar1.tier] += overlap_duration
-
-    for t1 in tier_names_targets:
-        print(t1 + " total: " + str(duration_totals[t1]))
-        for t2 in tier_names_targets:
-            print(t1 + "." + t2 + ": " + str(overlap_matrix[t1][t2]))
-
-    print("Total Duration: " + str(duration_total))
-    print("Total Overlap:  " + str(overlap_total))
-    print("Final Duration: " + str(duration_total - overlap_total))
-
-    # sys.argv[2] has the name of the output file 
-    # all writes are appended to the file
-    with open(sys.argv[2], "a") as writingFile:
-        writingFile.write('FileName,TierName,')
-        #write the name of all the tiers
-        for tier_name in tier_names_targets:
-            writingFile.write(tier_name + ",")
-            for annotation in eafob.get_annotation_data_for_tier(tier_name):
-                tier_dictionary[tier_name].append(str(annotation[0]) + "-" + str(annotation[1]))
-                #print(str(annotation[1]) +"-"+ str(annotation[0]))
-        writingFile.write("ADS,CDS,BOTH,TotalOverlap,Total\n")
-        
-        overlap_list=[]
-        keylist = tier_dictionary.keys()
-        keylist.sort()
-        for key in keylist:
-            total_time = 0
-            writingFile.write(os.path.basename(file_path).replace(".eaf", "") + "," + key + ",")
-            print(key+":",end='')
-            intersection_dict = dict()
-            total_intersection = 0
-            keylist2 = tier_dictionary.keys()
-            keylist2.sort()
-            for key2 in keylist2:
-                intersection_dict[key2] = 0
-                for value in tier_dictionary[key]:
-                    
-                    v1_begin = int(value.split('-')[0])
-                    v1_end   = int(value.split('-')[1])
-                    if v1_end < v1_begin:
-                        print("Error: duration negative")
-                    if key2 == key:
-                        total_time += v1_end - v1_begin
-
-                    else:
-                        #check other tiers
-                        for value2 in tier_dictionary[key2]:
-                            v2_begin = int(value2.split('-')[0])
-                            v2_end   = int(value2.split('-')[1])
-
-                            # first_end = min(v1_end, v2_end)
-                            # last_begin = max(v1_begin, v2_begin)
-                            # if (last_begin < first_end):
-                            #     intersection_dict[key2] += first_end - last_begin
-                            
-                            if v2_begin < v1_end and v2_begin >= v1_begin:
-                                if v2_end <= v1_end:
-                                    intersection_dict[key2] += v2_end - v2_begin
-                                    #print(key + " -- " + key2 + str(v2_end - v2_begin))
-                                else:
-                                    intersection_dict[key2] += v1_end - v2_begin
-                                    
-                            #target's end is encompassed by the original's begin and end
-                            elif v2_end > v1_begin and v2_end <= v1_end:
-                                #which one is larger? target's begin or original's
-                                #reverse logic as larger time needs to get subtracted
-                                if v2_begin <= v1_begin:
-                                    intersection_dict[key2] += v2_end - v1_begin
-                                else:
-                                    # this code is unreachable; covered above.
-                                    intersection_dict[key2] += v2_end - v2_begin
-
-                            elif v1_begin >= v2_begin and v1_end <= v2_end:
-                                intersection_dict[key2] += v1_end - v1_begin
-
-                if key2 != key:  
-                    #only add the overlapping time once.
-                    if (key2 + "-" + key not in overlap_list) and (key+"-"+key2 not in overlap_list):
-                        overlap_list.append(key2 + "-" + key)
-                        writingFile.write(str(intersection_dict[key2])+",") 
-                        total_intersection += intersection_dict[key2]
-                        print(key2 + "-" + str(intersection_dict[key2]) + ",", end = '')
-                    else:
-                        writingFile.write("0,")
-
-                else:
-                    writingFile.write("0,")
+    return union_sum, section_sums, sections
 
 
-            #ads cds part
-            ads  = 0
-            cds  = 0
-            both = 0
-            try:
-                if eafob.get_annotation_data_for_tier('xds@' + key):
-                    for annnotate in eafob.get_annotation_data_for_tier('xds@' + key):
-                        if annnotate[2] == 'C' :
-                            cds += annnotate[1]-annnotate[0]
-                        elif annnotate[2] == 'T':
-                            cds += annnotate[1]-annnotate[0]
-                        elif annnotate[2] == 'A':
-                            ads += annnotate[1]-annnotate[0]
-                        elif annnotate[2] == 'B':
-                            both +=annnotate[1]-annnotate[0]
-            except:
-                #since there are not method for checkign whether a  certain tier exists or not
-                ads  = 0
-                cds  = 0
-                both = 0
-            writingFile.write(str(ads) + "," + str(cds) + "," + str(both) + ",")
-            ads_total += ads
-            cds_total += cds
-            both_total += both
-            
-            
-            total_time -= total_intersection
-            total_intersection_accross += total_intersection
-            writingFile.write(str(total_intersection)+","+str(total_time)+"\n")
-            grand_total += total_time
-            print("total " + str(total_time))
-        writingFile.write(",")    
-        for tier_name in (tier_names_targets):
-                writingFile.write(",")
-        writingFile.write(",,,")
-        writingFile.write("Grand Total,"+str(grand_total)+",")
-        if grand_total<10*60*60*1000:
-            writingFile.write("Resample\n")
-        grand_total_accross+=grand_total
-        
-with open(sys.argv[2], "a") as writingFile:
-    writingFile.write("Grand Total,Grand Adjusted Total, ADS, CDS, BOTH\n");
-    writingFile.write(str(grand_total_accross+total_intersection_accross)+","+str(grand_total_accross)+","+ str(ads_total)+","+ str(cds_total)+","+ str(both_total)+"\n");
-    #break  # for one file
+def create_xds_table(tiers):
+    xds_tiers = filter(lambda tier: 'xds@' in tier, tiers)
+    return
+
+
+parser = argparse.ArgumentParser(description = """
+Analyze and report the time segments annotated in EAF files.
+""")
+parser.add_argument('--xds', action='store_true',
+                    help='summarize ADS & CDS amounts')
+parser.add_argument('--totals', metavar='TOTALS_CSV', type=argparse.FileType('w'),
+                    default='totals.csv',
+                    help='file to write to')
+parser.add_argument('--sep', metavar='CSV_SEPARATOR', default='\t',
+                    help='separator to use in the output file')
+parser.add_argument('--ignore', metavar='TIER', nargs='*',
+                    default=['code_num', 'on_off', 'context', 'code'],
+                    help='tiers to ignore')
+parser.add_argument('eaf_files', metavar='EAF_FILE', nargs='+',
+                    help='the name(s) of the EAF file(s) to process')
+args = parser.parse_args()
+
+
+for eaf_file in args.eaf_files:
+    file_id = os.path.basename(eaf_file).replace('.eaf', '')
+    eaf = pympi.Elan.Eaf(eaf_file)
+    tiers = filter(lambda tier: tier not in args.ignore,
+                   eaf.get_tier_names())
+    tiers = filter(lambda tier: '@' not in tier, tiers)
+    segments = get_records(eaf, tiers)
+    events = segments_to_events(segments)
+    (union_sum, section_sums, sections) = process_events(events)
+    labels = sorted(section_sums.keys())
+    labels.remove('')
+    for label in labels:
+        args.totals.write(
+            args.sep.join([file_id, label, str(section_sums[label])]) + '\n')
+    args.totals.write(args.sep.join([file_id, 'total', '', str(union_sum)]) + '\n')
+    for tier in tiers:
+        tier_sum = 0
+        for label in labels:
+            if tier in label:
+                tier_sum += section_sums[label]
+        args.totals.write(args.sep.join([file_id, tier, '', str(tier_sum)]) + '\n')
+    continue
+
+if args.xds:
+    ads_total = 0
+    cds_total = 0
+    both_total = 0
+    tiers = filter(lambda tier: tier not in args.ignore,
+                   eaf.get_tier_names())
+    xds_tiers = filter(lambda tier: 'xds@' in tier, tiers)
+    segments = get_records(eaf, xds_tiers)
+    events = segments_to_events(
+        segments, lambda x: x.tier.split('@')[-1] + ':' + x.value)
+    (union_sum, section_sums, sections) = process_events(events)
+    labels = sorted(section_sums.keys())
+    for label in labels:
+        args.totals.write(
+            args.sep.join([file_id, label, str(section_sums[label])]) + '\n')
+        codes = []
+        for tier_code in label.split('+'):
+            code = tier_code.split(':')[-1]
+            if code in codes:
+                continue
+            if code == 'A':
+                codes.append('A')
+                ads_total += section_sums[label]
+            elif code == 'B':
+                codes.append('B')
+                both_total += section_sums[label]
+            elif code == 'C' or code == 'T':
+                codes.append('C')
+                codes.append('T')
+                cds_total += section_sums[label]
+    args.totals.write(args.sep.join([file_id, 'ADS', '', str(ads_total)]) + '\n')
+    args.totals.write(args.sep.join([file_id, 'CDS', '', str(cds_total)]) + '\n')
+    args.totals.write(args.sep.join([file_id, 'BOTH', '', str(both_total)]) + '\n')
+
+
+args.totals.close()
+
+exit
